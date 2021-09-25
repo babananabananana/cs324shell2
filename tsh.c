52c87cdd3a28e3e2f849eb3219447b912ace1c62
Ethan Low <ethanthelow@gmail.com>
2021-09-24 19:26:07 -0600

    “hi”

--- tsh.c
+++ tsh.c
@@ -254,13 +254,9 @@ void eval(char *cmdline)
         int state = !bg ? FG:BG;
         sigprocmask(SIG_BLOCK, &mask_all, NULL);
         addjob(jobs, mypid, pid[0], state, cmdline);
-        sigprocmask(SIG_SETMASK, &prev_one, NULL);
 
-        sigprocmask(SIG_BLOCK, &mask_all, NULL);
         addjob(jobs, mypid, pid[0], state, cmdline);
-        sigprocmask(SIG_SETMASK, &prev_one, NULL);
 
-        sigprocmask(SIG_BLOCK, &mask_all, NULL);
         addjob(jobs, mypid, pid[0], state, cmdline);
         sigprocmask(SIG_SETMASK, &prev_one, NULL);
 
