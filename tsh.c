8b822a184ba8158cbb1fe0e7d03f004aed7a9bc2
Ethan Low <ethanthelow@gmail.com>
2021-09-28 00:05:47 -0600

    “hi”

--- tsh.c
+++ tsh.c
@@ -430,7 +430,7 @@ void do_bgfg(char **argv)
     char* cmdline;
 
     //error check
-    if (strlen(argv[1]) < 1){
+    if ((sizeof (argv)/sizeof argv[0])< 2){
         printf("%s command requires PID or %%job id argument\n", cmd);
         return;
     }
